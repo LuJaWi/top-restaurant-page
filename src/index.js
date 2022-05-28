243c93d23868eb74c52c045e99677d5715662c5a
Luke Witt <Witt.Luke@gmail.com>
2022-05-28 13:03:50 -0400

    webpack updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("We good.")
+import './style.css';
+import Icon from './assets/pizza-box.jpeg';
+
+function component () {
+  const element = document.createElement('div');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  return element;
+};
+
+document.body.appendChild(component())
