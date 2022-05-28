6c2bb5ea4836a6871ee4c6ab91c5acb318bdd358
Luke Witt <Witt.Luke@gmail.com>
2022-05-28 13:59:24 -0400

    Basic landing page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import './style.css';
 import Icon from './assets/pizza-box.jpeg';
 
-function component () {
+function homePage () {
   const element = document.createElement('div');
 
+  // Page image
   const myIcon = new Image();
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
 
+  // Page Title
+  const pageTitle = document.createElement('h1');
+  pageTitle.innerText = "We Eatsa Da Pizza";
+
+  element.appendChild(pageTitle);
+
+  // Page Content
+  const pageContent = document.createElement('p');
+  pageContent.innerText = "Lorem Ipsum? Uhhhhh, put it on the pizza.";
+
+  element.appendChild(pageContent);
+
   return element;
 };
 
-document.body.appendChild(component())
+const contentDivSelector = document.getElementById('content');
+
+contentDivSelector.appendChild(homePage());
