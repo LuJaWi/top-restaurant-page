ae6dbd0ec796f589b6df0a9ce6b0d540f61db69e
Luke Witt <Witt.Luke@gmail.com>
2022-05-30 18:09:45 -0400

    Updates to page layout

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import Icon from './assets/pizza-box.jpeg';
 
 function navBar () {
   const element = document.createElement('div');
+  element.classList.add('nav-bar')
 
   const homeButton = document.createElement('div');
+  homeButton.classList.add('nav-button');
   homeButton.innerText = 'Home';
 
   const menuButton = document.createElement('div');
+  menuButton.classList.add('nav-button');
   menuButton.innerText = 'Menu';
 
   const contactButton = document.createElement('div');
+  contactButton.classList.add('nav-button');
   contactButton.innerText = 'Contact';
 
   element.appendChild(homeButton);
@@ -23,18 +27,18 @@ function navBar () {
 function homePage () {
   const element = document.createElement('div');
 
-  // Page image
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
   // Page Title
   const pageTitle = document.createElement('h1');
   pageTitle.innerText = "We Eatsa Da Pizza";
 
   element.appendChild(pageTitle);
 
+  // Page image
+  const myIcon = new Image(300, 300);
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  
   // Page Content
   const pageContent = document.createElement('p');
   pageContent.innerText = "Lorem Ipsum? Uhhhhh, put it on the pizza.";
